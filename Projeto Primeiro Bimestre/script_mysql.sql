a3d9fd82863f759ae0fbc5f849c9bc9a502827de
Natan Depes <103089417+natandepes@users.noreply.github.com>
2023-03-28 20:17:55 -0300

    Colocando a questão 3 e adicionando coluna na 2

--- Projeto Primeiro Bimestre/script_mysql.sql
+++ Projeto Primeiro Bimestre/script_mysql.sql
@@ -21,16 +21,39 @@ INNER JOIN customer cs ON (cs.cust_id = bi.cust_id);
 -- das pessoas físicas) que possuem uma conta em uma cidade diferente da cidade de estabelecimento          --                                                    --
 -- -------------------------------------------------------------------------------------------------------- --
 
-SELECT DISTINCT CONCAT(ind.fname,' ', ind.lname) AS Clients
+SELECT DISTINCT 
+     'FIS' Type
+,    CONCAT(ind.fname,' ', ind.lname) AS Clients
 FROM individual     ind
 INNER JOIN customer cs  ON (cs.cust_id = ind.cust_id)
 INNER JOIN account  acc ON (acc.cust_id = cs.cust_id)
 INNER JOIN branch   bc  ON (bc.branch_id = acc.open_branch_id)
 WHERE cs.city != bc.city
 UNION
-SELECT DISTINCT bi.name AS Clients
+SELECT DISTINCT 
+      'FIS' Type
+,     bi.name AS Clients
 FROM business 	    bi
 INNER JOIN customer cs  ON (cs.cust_id = bi.cust_id)
 INNER JOIN account  acc ON (acc.cust_id = cs.cust_id)
 INNER JOIN branch   bc  ON (bc.branch_id = acc.open_branch_id)
 WHERE cs.city != bc.city;
+
+
+
+
+
+-- ------------------------------------------------------------------------------------------------------ --
+-- 3) Escreva uma consulta que retorne os nome de todos os funcionários com, se for o caso, os números de --
+-- transações por ano envolvendo as contas que eles abriram (usando open_emp_id). Ordene os resultados    --
+-- por ordem alfabética, e depois por ano (do mais antigo para o mais recente).                           --                            
+-- ------------------------------------------------------------------------------------------------------ --
+
+SELECT
+	CONCAT(emp.fname,' ', emp.lname) AS Name
+, COUNT(trn.txn_id) 				       AS Transactions
+FROM employee 		    emp
+LEFT JOIN account 	  acc ON (acc.open_emp_id = emp.emp_id)
+LEFT JOIN transaction trn ON (trn.account_id = acc.account_id)
+GROUP BY Name
+ORDER BY Name, trn.txn_date;
